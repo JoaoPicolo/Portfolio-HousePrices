5abf71c6182b6837f489e50ad3e7c2b26b6c3e51
João Pedro Picolo <picolo@Joaos-MacBook-Pro.local>
2023-02-19 16:54:47 -0300

    Updated bloxplot visualization approach

--- project/src/run.py
+++ project/src/run.py
@@ -1,5 +1,6 @@
 from data.cleaning import grade_formatting
 from data.utils import load_data_to_dataframe, save_data_to_csv
+from visualization.exploration import get_outliers_plot, get_correlation_plot
 
 
 def main():
@@ -10,8 +11,9 @@ def main():
         print("It was not possible to read the provided .csv file")
         exit(0)
 
-    cleaned_df = grade_formatting(dataframe)
-    save_data_to_csv(cleaned_df, data_path="../data/interim/kc_house_data.csv")
+    get_outliers_plot(dataframe, drop_columns=["id", "date", "waterfront", "lat", "long", "zipcode", "yr_renovated"])
+    # cleaned_df = grade_formatting(dataframe)
+    # save_data_to_csv(cleaned_df, data_path="../data/interim/kc_house_data.csv")
 
 
 if __name__ == "__main__":
--- project/src/visualization/exploration.py
+++ project/src/visualization/exploration.py
@@ -1,8 +1,39 @@
+import math
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 def get_correlation_plot(dataframe: pd.DataFrame):
+    """ Shows a correlation matrix for the dataframe
+
+    Parameters:
+    dataframe: Dataframe on which the operation will be performed
+    """
     sns.heatmap(dataframe.corr(method="pearson"), annot=True)
     plt.title("Pearson's correlation coefficients")
     plt.show()
+
+
+def get_outliers_plot(dataframe: pd.DataFrame, drop_columns: list[str]):
+    """ Shows a boxplot for each columns in the dataframe
+
+    Parameters:
+    dataframe: Dataframe on which the operation will be performed
+    drop_columns: Columns to ignore when creating the boxplots
+    """
+    cleaned_df = dataframe.drop(drop_columns, axis=1)
+    columns = cleaned_df.columns
+
+    ncols = 2
+    nrows = math.ceil(len(columns)/2)
+    _, axes = plt.subplots(nrows, ncols, figsize=(20,40)) # creating a figure with 2 rows and 2 columns of plots
+    for index, col in enumerate(columns):
+        ax = axes[math.floor(index/2), index%2] # selecting the current subplot
+        plot = sns.boxplot(y=cleaned_df[col], ax=ax)
+        plot.set_title(f"{col} Distribution")
+        plot.set_ylabel("")
+        plot.ticklabel_format(style='plain', axis='y')
+
+    plt.tight_layout() # adjusting the spacing between subplots
+    plt.show() # displaying the final figure
+
