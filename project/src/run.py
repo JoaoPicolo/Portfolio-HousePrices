456eb2638c8a3d26c95f6bf96c3f216792321dfd
João Pedro Picolo <picolo@Joaos-MacBook-Pro.local>
2023-02-19 18:33:09 -0300

    Added visualization methods

--- project/src/run.py
+++ project/src/run.py
@@ -1,6 +1,5 @@
 from data.cleaning import grade_formatting
 from data.utils import load_data_to_dataframe, save_data_to_csv
-from visualization.exploration import get_outliers_plot, get_correlation_plot
 
 
 def main():
@@ -11,9 +10,8 @@ def main():
         print("It was not possible to read the provided .csv file")
         exit(0)
 
-    get_outliers_plot(dataframe, drop_columns=["id", "date", "waterfront", "lat", "long", "zipcode", "yr_renovated"])
-    # cleaned_df = grade_formatting(dataframe)
-    # save_data_to_csv(cleaned_df, data_path="../data/interim/kc_house_data.csv")
+    cleaned_df = grade_formatting(dataframe)
+    save_data_to_csv(cleaned_df, data_path="../data/interim/kc_house_data.csv")
 
 
 if __name__ == "__main__":
--- project/src/visualization/exploration.py
+++ project/src/visualization/exploration.py
@@ -14,26 +14,53 @@ def get_correlation_plot(dataframe: pd.DataFrame):
     plt.show()
 
 
-def get_outliers_plot(dataframe: pd.DataFrame, drop_columns: list[str]):
+def get_outliers_plot(dataframe: pd.DataFrame, ignore_columns: list[str] = []):
     """ Shows a boxplot for each columns in the dataframe
 
     Parameters:
     dataframe: Dataframe on which the operation will be performed
-    drop_columns: Columns to ignore when creating the boxplots
+    ignore_columns: Columns to ignored when creating the boxplots
     """
-    cleaned_df = dataframe.drop(drop_columns, axis=1)
+    cleaned_df = dataframe.drop(ignore_columns, axis=1)
     columns = cleaned_df.columns
 
     ncols = 2
     nrows = math.ceil(len(columns)/2)
-    _, axes = plt.subplots(nrows, ncols, figsize=(20,40)) # creating a figure with 2 rows and 2 columns of plots
-    for index, col in enumerate(columns):
-        ax = axes[math.floor(index/2), index%2] # selecting the current subplot
-        plot = sns.boxplot(y=cleaned_df[col], ax=ax)
-        plot.set_title(f"{col} Distribution")
-        plot.set_ylabel("")
-        plot.ticklabel_format(style='plain', axis='y')
+    _, axes = plt.subplots(nrows, ncols, figsize=(20,40))
+    for index, axis in enumerate(axes.reshape(-1)):
+        if index < len(columns):
+            plot = sns.boxplot(y=cleaned_df[columns[index]], ax=axis)
+            plot.set_title(f"{columns[index]} Values")
+            plot.set_ylabel("")
+            plot.ticklabel_format(style='plain', axis='y')
+        else: # If number of plots is odd, don't print the last one since it's expecting an even number of plots
+            axis.axis('off')
 
     plt.tight_layout() # adjusting the spacing between subplots
     plt.show() # displaying the final figure
+
+
+def get_distribution_plot(dataframe: pd.DataFrame, ignore_columns: list[str] = []):
+    """ Shows a distribution plot for each columns in the dataframe
+
+    Parameters:
+    dataframe: Dataframe on which the operation will be performed
+    ignore_columns: Columns to ignored when creating the distribution plots
+    """
+    cleaned_df = dataframe.drop(ignore_columns, axis=1)
+    columns = cleaned_df.columns
+
+    ncols = 2
+    nrows = math.ceil(len(columns)/2)
+    _, axes = plt.subplots(nrows, ncols, figsize=(20,40))
+    for index, axis in enumerate(axes.reshape(-1)):
+        if index < len(columns):
+            plot = sns.histplot(x=cleaned_df[columns[index]], ax=axis)
+            plot.set_title(f"{columns[index]} Distribution")
+            plot.ticklabel_format(style='plain', axis='x')
+        else: # If number of plots is odd, don't print the last one since it's expecting an even number of plots
+            axis.axis('off')
+
+    plt.tight_layout() # adjusting the spacing between subplots
+    plt.show # displaying the final figure
 
