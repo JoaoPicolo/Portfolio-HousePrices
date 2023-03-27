4bf04316ed4ee9dbbb8a2a303598c744381b0dd5
João Pedro Picolo <picolo@Joaos-MacBook-Pro.local>
2023-03-27 13:49:21 -0300

    Added results visualization

--- src/model_build.py
+++ src/model_build.py
@@ -2,12 +2,13 @@ from models.utils import evaluate_cv_search
 from models.model_build import xgboost_train, xgboost_test
 from data.utils import load_data_to_dataframe, get_train_test_data
 
-from visualization.analysis import plot_prediction
+from visualization.analysis import plot_prediction, plot_feature_importance
+
 
 def main():
     datasets = ["../data/processed/kc_house_data.csv",
                 "../data/processed/kc_house_data_no_outlier.csv"]
-
+    
     for dataset in datasets:
         try:
             dataframe = load_data_to_dataframe(data_path=dataset)
@@ -17,14 +18,11 @@ def main():
 
         print(f"\n\nAnalysing {dataset.split('/')[3]}")
 
-
         X_train, X_test, y_train, y_test = get_train_test_data(dataframe, ["price"])
 
-       
         xgboost_search = xgboost_train(X_train, y_train, n_iter=10)
         evaluate_cv_search(xgboost_search)
-        y_pred = xgboost_test(xgboost_search, X_test, y_test)
-        plot_prediction(y_test, y_pred)
+        xgboost_test(xgboost_search, X_test, y_test)
 
 
 if __name__ == "__main__":
--- src/visualization/analysis.py
+++ src/visualization/analysis.py
@@ -5,8 +5,10 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from typing import List
 
-def plot_prediction(y_true: np.ndarray, y_pred: np.ndarray):
+
+def plot_prediction(y_true: np.ndarray, y_pred: np.ndarray, image):
     y_true = np.squeeze(y_true)
     values = np.concatenate((y_true, y_pred), axis=0)
     labels = np.concatenate((["True"] * len(y_true), ["Predicted"] * len(y_pred)), axis=0)
@@ -18,7 +20,19 @@ def plot_prediction(y_true: np.ndarray, y_pred: np.ndarray):
     true_values = results[results["Label"] == "True"].reset_index()
     false_values = results[results["Label"] == "Predicted"].reset_index()
 
+    plt.clf()
     sns.lineplot(data=true_values, x=true_values.index, y="Value", color="red", label="True")
     sns.lineplot(data=false_values, x=false_values.index, y="Value", color="blue", label="Predicted")
     plt.title("True Values x Predicted Values")
-    plt.show()
+    plt.savefig(image)
+
+
+def plot_feature_importance(feature_importances: List[float], columns: List[str], image):
+    importances = pd.DataFrame({
+        "Importances": feature_importances,
+        "Features": columns
+    })
+
+    plt.clf()
+    sns.barplot(data=importances, x="Importances", y="Features")
+    plt.savefig(image)
